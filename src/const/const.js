c6b96ce79fbdbc86c59114d9d59ed49e4083e460
vv124852 <vishal.v@esmagico.in>
2025-01-27 15:06:52 +0530

    feat: Fetching stories from API and display on web

---
+++ src/const/const.js
@@ -0,0 +1,4 @@
+export const playstore_deeplink =
+  "https://play.google.com/store/apps/details?id=com.esmagico.pawfect&hl=en";
+export const appstore_deeplink =
+  "https://apps.apple.com/in/app/the-pawfect-date/id6474994092";
