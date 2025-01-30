15ea06f85b746f62221d22270a49909856d75ff0
vv124852 <vishal.v@esmagico.in>
2025-01-30 17:05:39 +0530

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ export default {
       screens: {
         xs: "360px", // Adjusted for smaller phones
         sm: "480px", // Small devices
-        md: "768px", // Medium devices
+        md: "740px", // Medium devices
+        m: "540px",
         lg: "1024px", // Large devices
         xl: "1280px", // Extra large devices
         "2xl": "1536px", // 2X Large devices
